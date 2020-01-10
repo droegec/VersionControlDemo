6067ca4c11fb5800b3d8cccfdc08cdb49c694a9f
Christoph Droege <Christoph.Droege@akka.eu>
2020-01-10 10:35:06 +0100

    Meine erste Versionierung

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println ("Hallo Welt");
+	    // Das ist mein Test
+        System.out.println ("Hallo Welt");
+        System.out.println ("Hallo Welt");
+
     }
 }
